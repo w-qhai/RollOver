04bb17236b70d0ec77771b6f86a1cd96b849ee28
QXQZX <501966782@qq.com>
2020-08-06 14:08:21 +0800

    添加冒险模式关卡选择

---
+++ SourceCode/VCProject2015/VCProject2015/LevelChooser.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "LessonX.h"
+extern long double fTimeDelta;
+
+namespace LevelChooser {
+    extern bool			left_pressed;   // 鼠标左键是否按下
+    extern CStaticSprite level_1;	    // 第一关
+
+    void		OnMouseMove(const float fMouseX, const float fMouseY);
+    void		OnMouseClick(const int iMouseType, const float fMouseX, const float fMouseY);
+    void		OnMouseUp(const int iMouseType, const float fMouseX, const float fMouseY);
+    void		OnKeyDown(const int iKey, const bool bAltPress, const bool bShiftPress, const bool bCtrlPress);
+    void		OnKeyUp(const int iKey);
+    void		OnSpriteColSprite(const char* szSrcName, const char* szTarName);
+    void		OnSpriteColWorldLimit(const char* szName, const int iColSide);
+};
+
